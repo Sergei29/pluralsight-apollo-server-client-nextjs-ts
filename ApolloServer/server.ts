2ea655895a9c68d811a5e16a6c27255f8aa4442b
Serge Basangovs <sergejs.basangovs@gmail.com>
2021-04-25 14:06:24 +0300

    server: implemented schema for sessionsConcat

--- ApolloServer/server.ts
+++ ApolloServer/server.ts
@@ -1,6 +1,11 @@
 import { gql, ApolloServer, UserInputError, IResolvers } from "apollo-server";
 import axios from "axios";
-import { SpeakerType, PageInfoType } from "./types";
+import {
+  SpeakerType,
+  PageInfoType,
+  SessionType,
+  SessionSpeakerType,
+} from "./types";
 
 const DB_URI = "http://localhost:5000";
 
@@ -21,6 +26,7 @@ const typeDefs = gql`
     last: String
     favorite: Boolean
     cursor: String
+    sessions: [Session]
   }
 
   input SpeakerInput {
@@ -40,9 +46,22 @@ const typeDefs = gql`
     pageInfo: PageInfo
   }
 
+  type Session {
+    id: ID!
+    title: String!
+    eventYear: String
+    cursor: String
+  }
+
+  type SessionResults {
+    datalist: [Session]
+    pageInfo: PageInfo
+  }
+
   type Query {
     speakers(offset: Int! = 0, limit: Int! = -1): SpeakerResults
     speakersConcat(limit: Int = -1, afterCursor: String = ""): SpeakerResults
+    sessionsConcat(limit: Int = -1, afterCursor: String = ""): SessionResults
   }
 
   type Mutation {
@@ -71,7 +90,7 @@ const resolvers: IResolvers = {
       const { data: arrAllSpeakers } = await axios.get(`${DB_URI}/speakers`);
 
       const arrSortedByName = (arrAllSpeakers as SpeakerType[]).sort(
-        (objCurrent, objNext) => objCurrent.last.localeCompare(objNext.last)
+        (objCurrent, objNext) => objCurrent.last!.localeCompare(objNext.last!)
       );
 
       const offsetIndex = getOffsetCustom(arrSortedByName, afterCursor);
@@ -98,6 +117,66 @@ const resolvers: IResolvers = {
       };
 
       return { datalist, pageInfo };
+    },
+    sessionsConcat: async (parent, args, context, info) => {
+      const { limit, afterCursor } = args;
+      const { data: arrAllSessions } = await axios.get(`${DB_URI}/sessions`);
+
+      const arrSortedByName = (arrAllSessions as SessionType[]).sort(
+        (objCurrent, objNext) =>
+          objCurrent.eventYear!.localeCompare(objNext.eventYear!)
+      );
+
+      const offsetIndex = getOffsetCustom(arrSortedByName, afterCursor);
+
+      const datalist = (arrAllSessions as SessionType[])
+        .filter((_, index) => {
+          return (
+            index > offsetIndex - 1 &&
+            (offsetIndex + limit > index || limit === 1)
+          );
+        })
+        .map((objSession) => {
+          objSession.cursor = getCursor(objSession.id);
+          return objSession;
+        });
+
+      const pageInfo: PageInfoType = {
+        totalItemCount: arrAllSessions.length,
+        lastCursor:
+          datalist.length > 0
+            ? getCursor(datalist[datalist.length - 1].id)
+            : "",
+        hasNextPage: offsetIndex + datalist.length < arrAllSessions.length,
+      };
+
+      return { datalist, pageInfo };
+    },
+  },
+  Speaker: {
+    sessions: async (parent, args, context, info) => {
+      const speakerId = parent.id;
+      const responseSessions = await axios.get(`${DB_URI}/sessions`);
+      const responseSessionSpeakers = await axios.get(
+        `${DB_URI}/sessionSpeakers`
+      );
+
+      const sessionIds: String[] = responseSessionSpeakers.data
+        .filter(
+          (objSessionSpeaker: SessionSpeakerType) =>
+            objSessionSpeaker.speakerId === speakerId
+        )
+        .map(
+          (objSessionSpeaker: SessionSpeakerType) => objSessionSpeaker.sessionId
+        );
+
+      const speakerSessionsResult: SessionType[] = responseSessions.data
+        .filter((objSession: SessionType) => sessionIds.includes(objSession.id))
+        .sort((objCurrent: SessionType, objNext: SessionType) =>
+          objCurrent.eventYear!.localeCompare(objNext.eventYear!)
+        );
+
+      return speakerSessionsResult;
     },
   },
   Mutation: {
--- ApolloServer/types.ts
+++ ApolloServer/types.ts
@@ -1,9 +1,23 @@
+export type SessionSpeakerType = {
+  sessionId: number;
+  speakerId: number;
+  eventYear: string;
+};
+
+export type SessionType = {
+  id: string;
+  title: string;
+  eventYear?: string;
+  cursor?: string;
+};
+
 export type SpeakerType = {
   id: string;
-  first: string;
-  last: string;
-  favorite: boolean;
+  first?: string;
+  last?: string;
+  favorite?: boolean;
   cursor?: string;
+  sessions?: SessionType[];
 };
 
 export type PageInfoType = {
